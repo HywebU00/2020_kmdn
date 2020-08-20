5c67a56223da66f7c5fc3f301bc29af63608318a
Miley <miley.ho@hyweb.com.tw>
2020-08-20 16:40:02 +0800

    新增 weather icon

--- js/hyui.js
+++ js/hyui.js
@@ -110,6 +110,7 @@ $(function() {
         ww = _window.outerWidth();
         wwMedium = 992;
 
+        //廣告banner切換
         if (ww < 992){
             $('.banner_area').prependTo('.top_block .container');
         }else{
@@ -519,26 +520,26 @@ $(function() {
     /*------------------------------------*/
     //////////分享按鈕 share dropdwon////////
     /*------------------------------------*/
-    $('.function_panel .share').children('ul').hide();
-    $('.function_panel .share').prepend('<a href="#" class="shareButton">share分享按鈕</a>');
-    var _shareButton = $('.shareButton');
-    _shareButton.off().click(function(e) {
-        $(this).siblings('ul').stop(true, true).slideToggle();
-        e.preventDefault();
-    });
-    _shareButton.keyup(function(event) {
-        $(this).siblings('ul').stop(true, true).slideDown();
-    });
-    $('.function_panel .share').find('li:last>a').focusout(function(event) {
-        $(this).parent().parent('ul').hide();
-    });
-    // 點外面關閉share
-    $(document).on('touchend click', function(e) {
-        var container = $(".function_panel .share");
-        if (!container.is(e.target) && container.has(e.target).length === 0) {
-            $('.function_panel .share ul').hide();
-        }
-    });
+    // $('.function_panel .share').children('ul').hide();
+    // $('.function_panel .share').prepend('<a href="#" class="shareButton">share分享按鈕</a>');
+    // var _shareButton = $('.shareButton');
+    // _shareButton.off().click(function(e) {
+    //     $(this).siblings('ul').stop(true, true).slideToggle();
+    //     e.preventDefault();
+    // });
+    // _shareButton.keyup(function(event) {
+    //     $(this).siblings('ul').stop(true, true).slideDown();
+    // });
+    // $('.function_panel .share').find('li:last>a').focusout(function(event) {
+    //     $(this).parent().parent('ul').hide();
+    // });
+    // // 點外面關閉share
+    // $(document).on('touchend click', function(e) {
+    //     var container = $(".function_panel .share");
+    //     if (!container.is(e.target) && container.has(e.target).length === 0) {
+    //         $('.function_panel .share ul').hide();
+    //     }
+    // });
     /*------------------------------------*/
     /////////////字型大小 font-size//////////
     /*------------------------------------*/

5e8463a6a14b7e2be30921c970d30fad70e8dd89
Miley <miley.ho@hyweb.com.tw>
2020-08-31 11:29:31 +0800

    移除註解、調整css

--- js/customize.js
+++ js/customize.js
@@ -237,22 +237,16 @@ $(function() {
 
     var _showTab = 0;
     $('.m_tabSet').each(function(){
-        // 目前的頁籤區塊
         var $tab = $(this);
  
         var $defaultLi = $('.m_tabs ul li', $tab).eq(_showTab).addClass('active');
         $($defaultLi.find('a').attr('href')).siblings().hide();
  
-        // 當 li 頁籤被點擊時...
-        // 若要改成滑鼠移到 li 頁籤就切換時, 把 click 改成 mouseover
         $('.m_tabs ul li', $tab).click(function() {
-            // 找出 li 中的超連結 href(#id)
             var $this = $(this),
                 _clickTab = $this.find('a').attr('href');
-            // 把目前點擊到的 li 頁籤加上 .active
-            // 並把兄弟元素中有 .active 的都移除 class
             $this.addClass('active').siblings('.active').removeClass('active');
-            // 淡入相對應的內容並隱藏兄弟元素
+
             $(_clickTab).stop(false, true).fadeIn().siblings().hide();
  
             return false;
@@ -260,6 +254,8 @@ $(function() {
             this.blur();
         });
     });
+
+
 });
 
 
